409b04ad10f8d84dd987afc965730c8f25daed92
Brena Paiva <bpaiva@uniaoquimica.com.br>
2024-02-07 12:11:48 -0300

    first commit

---
+++ src/app.tsx
@@ -0,0 +1,34 @@
+import logo from './assets/Logo-nlw-expert.svg'
+import { NewCard } from './components/new-note-card'
+import { NoteCard } from './components/note-card'
+
+export function App() {
+  return (
+    <div className="mx-auto max-w-6xl my-12 space-y-6">
+    <img src={logo} alt='NLW Expert'/>
+    <form className="w-full">
+    <input 
+    className='w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500'
+    type='text'
+    placeholder='Busque em suas notas...'
+    />
+    </form>
+
+    <div className="h-px bg-slate-700"/>
+
+    <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+      <NewCard/>
+      <NoteCard/>
+      <NoteCard/>
+      <NoteCard/>
+      <NoteCard/>
+      <NoteCard/>
+      <NoteCard/>
+      <NoteCard/>
+
+    </div>
+    </div>
+  )
+}
+
+
