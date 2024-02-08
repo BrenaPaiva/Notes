026114d09aaa2aea1fae6f65e301fd3dda67df8b
Brena Paiva <bpaiva@uniaoquimica.com.br>
2024-02-07 23:06:03 -0300

    modificação de interface com Radix

--- src/app.tsx
+++ src/app.tsx
@@ -18,13 +18,12 @@ export function App() {
 
     <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
       <NewCard/>
-      <NoteCard/>
-      <NoteCard/>
-      <NoteCard/>
-      <NoteCard/>
-      <NoteCard/>
-      <NoteCard/>
-      <NoteCard/>
+      <NoteCard note={{
+        date: new Date(),
+        content:'Hello word'
+      }}/>
+     
+     
 
     </div>
     </div>
